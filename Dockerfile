9ff312dba10cd156778e4d2ca97976c02aae34cf
chvois1 <chr.voisin@orange.fr>
2024-05-23 17:35:34 +0200

    Project init

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM eclipse-temurin:17-jdk-jammy
-COPY kafka-dix-1.0-jar-with-dependencies.jar /opt/app/app.jar
+
+RUN mkdir /opt/app
+COPY target/com.kafka.stream-1.0-jar-with-dependencies.jar /opt/app/app.jar
 CMD ["java", "-jar", "/opt/app/app.jar", "kafka:9092", "zk:2181"]
---
+++ src/main/java/com/kafka/stream/model/ProduitBrut.java
@@ -0,0 +1,25 @@
+package com.kafka.stream.model;
+
+import java.math.BigDecimal;
+
+public class ProduitBrut {
+
+    private Long id;
+    private BigDecimal price;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
