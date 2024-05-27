11337b4b495619e9d81c082c1c64accaae0bb98f
chvois1 <chr.voisin@orange.fr>
2024-05-27 09:52:27 +0200

    README upadte

--- docker/kafka/scripts/inject-demandes.sh
+++ docker/kafka/scripts/inject-demandes.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo '{"id": 1, "price": 3.45}
-{"id": 2, "price": 13.40}
-{"id": 30, "price": 1.05}
-{"id": 1, "price": 3.40}' | $KAFKA_HOME/bin/kafka-console-producer --bootstrap-server localhost:9092 localhost:9092 --topic achats
+echo '{"id": 1, "name": "usager", "firstName": "usr01"}
+{"id": 2, "name": "usager", "firstName": "usr02"}
+{"id": 30, "name": "usager", "firstName": "usr01"}
+{"id": 1, "name": "usager", "firstName": "usr-01"}' | $KAFKA_HOME/bin/kafka-console-producer --bootstrap-server localhost:9092 localhost:9092 --topic achats
--- docker/kafka/scripts/inject-usagers.sh
+++ docker/kafka/scripts/inject-usagers.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo '1,{"id":1, "name":"produit1"}
-2,{"id":2, "name":"produit2"}
-3,{"id":3, "name":"produit3"}' | $KAFKA_HOME/bin/kafka-console-producer --bootstrap-server localhost:9092 localhost:9092 --topic referentiel --property parse.key=true --property key.separator=,
+echo '1,{"id":1, "eMail":"usager.001@mail.com"}
+2,{"id":2, "eMail":"usager.002@mail.com""}
+3,{"id":3, "eMail":"usager.003@mail.com""}' | $KAFKA_HOME/bin/kafka-console-producer --bootstrap-server localhost:9092 localhost:9092 --topic referentiel --property parse.key=true --property key.separator=,
 
